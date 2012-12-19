e59db406c9448ac39365e07fb5d4b2f78519bf15
francesquini <emilio.francesquini@imag.fr>
2012-12-19 19:19:38 +0100

    Agora vai

--- legendas.py
+++ legendas.py
@@ -1,14 +1,33 @@
+#import httplib, urllib
+#params = urllib.urlencode(dict(txtLogin="mbcastro", txtSenha="syntricks"))
+#headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+#conn = httplib.HTTPConnection("legendas.tv")
+#conn.request("POST", "/login_verificar.php", params, headers)
+#response = conn.getresponse()
+#print response.status, response.reason
+#data = response.read()
+#print data
+#conn.close()
+
+
 from httplib2 import Http
+import urllib
 from urllib import urlencode
+
 h = Http()
-data = {'txtLogin': "mbcastro", 'txtSenha': "syntricks", 'entrar.x':"20", 'entrar.y': "10"}
-resp, content = h.request("http://legendas.tv/login_verificar.php", "POST", urlencode(data))
+data = dict(txtLogin="mbcastro", txtSenha="syntricks")
+headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
-print resp
-print content
+resp, content = h.request("http://legendas.tv/login_verificar.php", method="POST", body=urlencode(data), headers=headers)
 
-#data = dict(txtLegenda="big bang theory", selTipo="1", int_idioma="1")
-#resp, content = h.request("http://legendas.tv/index.php?opcao=buscarlegenda", "POST", urlencode(data))
+#print resp
+#print content
+
+data = ["txtLegenda=big bang theory", "selTipo=1", "int_idioma=1"]
+print urllib.quote(data, '')
+resp, content = h.request("http://legendas.tv/index.php?opcao=buscarlegenda", "POST", urlencode(data),headers)
+
+
 
 #print resp
 #print content
