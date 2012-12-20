88b4396ca7367926b40d32bbe10249521d892dc8
Márcio Castro <mbcastro@gmail.com>
2012-12-20 16:44:34 +0100

    Versão funcional !
    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- legendas.py
+++ legendas.py
@@ -1,46 +1,45 @@
-#import httplib, urllib
-#params = urllib.urlencode(dict(txtLogin="mbcastro", txtSenha="syntricks"))
-#headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-#conn = httplib.HTTPConnection("legendas.tv")
-#conn.request("POST", "/login_verificar.php", params, headers)
-#response = conn.getresponse()
-#print response.status, response.reason
-#data = response.read()
-#print data
-#conn.close()
+import requests
+import re
+import os
+import sys
 
+query = sys.argv[1]
 
-from httplib2 import Http
-import urllib
-import html5lib
-from html5lib import treebuilders, treewalkers, serializer
-from html5lib.filters import sanitizer
-
-
-h = Http()
-data = dict(txtLogin="mbcastro", txtSenha="syntricks")
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-resp, content = h.request("http://legendas.tv/login_verificar.php", "POST", urllib.urlencode(data), headers)
+payload_login = {'txtLogin': 'mbcastro', 'txtSenha': 'syntricks'}
+request_login = requests.post("http://legendas.tv/login_verificar.php", data=payload_login, headers=headers)
 
-#Assert login successful
+payload_search = {'opcao': 'buscarlegenda', 'txtLegenda': query, 'selTipo': '1', 'int_idioma': '1'}
+request_search = requests.post("http://legendas.tv/index.php", data=payload_search, headers=headers, cookies=request_login.cookies)
+
+expression = r"span onmouseover=\"this\.T_OPACITY=95; this\.T_WIDTH=400; return escape\(gpop\('.+?','.+?','.+?'.+?javascript:abredown\('(.+?)'\).+?Release: \<span class=\"brls\"\>(.+?)\</span\>"
+result = re.findall(expression, request_search.text.replace('\n', ' ').replace('\r', ' '))
+
+for code, name in result:
+    request_subtitle = requests.get("http://legendas.tv/info.php?d=" + code + "&c=1", headers=headers, cookies=request_login.cookies, allow_redirects=False)
+    filename = request_subtitle.headers['location']
+
+    request_file = requests.get("http://legendas.tv/" + filename, headers=headers, cookies=request_login.cookies, stream=True, allow_redirects=False)
+ 
+    filename = "subtitles/" + filename[filename.rfind('/')+1:]
+    f = open(filename, 'wb')
+    f.write(request_file.raw.read())
+    f.close()
+    
+    file_type = filename[filename.rfind('.')+1:].lower()
+    if file_type == "zip":
+        os.system('unzip -o ' + filename + " -d subtitles/")
+    else:
+        if file_type == "rar":
+            os.system('unrar e -y ' + filename + " subtitles/")
+        else:
+            print ""
 
 
-fields =["opcao", "txtLegenda", "selTipo", "int_idioma"]
-values = map(lambda a: urllib.quote(a, ''), ["buscarlegenda", "big bang theory", "1", "1"])
-encodedData = reduce (
-    lambda a, b: a + '&' + b, 
-    [field + '=' + value for (field, value) in zip(fields, values)])
-resp, content = h.request("http://legendas.tv/index.php", "GET", encodedData, headers)
+#print request_subtitle.status_code
+#print request_subtitle.history
+#print request_subtitle.headers
+#print request_subtitle.content
 
-print content
-#Assert response successful
 
-parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("dom"))
-dom_tree = parser.parse(content)
-walker = treewalkers.getTreeWalker("dom")
-stream = walker(dom_tree)
-serializer = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False)
-output_generator = serializer.serialize(stream)
 
-#for item in output_generator:
-#    print item
