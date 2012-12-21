c884d599ccec6bc8393c474ed025b6036e740b1b
Márcio Castro <mbcastro@gmail.com>
2012-12-21 10:06:46 +0100

    Comentários no código. Adicionada pasta de legendas.

--- legendas.py
+++ legendas.py
@@ -3,18 +3,30 @@ import re
 import os
 import sys
 
+#login and password
+login = 'yourloginhere'
+password = 'yourpasswordhere' 
+
+#input parameter: example "big bang theory S03 E05"
 query = sys.argv[1]
 
+#set HTML headers
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-payload_login = {'txtLogin': 'mbcastro', 'txtSenha': 'syntricks'}
+
+#login on legendas.tv
+payload_login = {'txtLogin': login, 'txtSenha': password}
 request_login = requests.post("http://legendas.tv/login_verificar.php", data=payload_login, headers=headers)
 
+#search for subtitles using 'query'
 payload_search = {'opcao': 'buscarlegenda', 'txtLegenda': query, 'selTipo': '1', 'int_idioma': '1'}
 request_search = requests.post("http://legendas.tv/index.php", data=payload_search, headers=headers, cookies=request_login.cookies)
 
+#parse results
 expression = r"span onmouseover=\"this\.T_OPACITY=95; this\.T_WIDTH=400; return escape\(gpop\('.+?','.+?','.+?'.+?javascript:abredown\('(.+?)'\).+?Release: \<span class=\"brls\"\>(.+?)\</span\>"
 result = re.findall(expression, request_search.text.replace('\n', ' ').replace('\r', ' '))
 
+#for each result on the first page, download the corresponding subtitles and unzip/unrar them
+#todo: consider other pages?
 for code, name in result:
     request_subtitle = requests.get("http://legendas.tv/info.php?d=" + code + "&c=1", headers=headers, cookies=request_login.cookies, allow_redirects=False)
     filename = request_subtitle.headers['location']
@@ -33,5 +45,5 @@ for code, name in result:
         if file_type == "rar":
             os.system('unrar e -y -inul ' + filename + " subtitles/")
         else:
-            print ""
+            print "error: can't recognize file extention: " + file_type
 
